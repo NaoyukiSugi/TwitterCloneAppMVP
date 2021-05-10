fcc66d744b2fdfdac30b4ca1b8cbaef843a33f7d
shishamo <bnsgny@gmail.com>
2021-05-10 21:24:58 +0900

    move models (#5)

---
+++ common_api/src/main/java/com/example/common_api/home_timeline/HomeTimelineApi.kt
@@ -0,0 +1,9 @@
+package com.example.common_api.home_timeline
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface HomeTimelineApi {
+    @GET("1.1/statuses/home_timeline.json")
+    suspend fun getHomeTimeline(): Response<List<Tweet>>
+}
--- common_api/src/main/java/com/example/common_api/home_timeline/Tweet.kt
+++ common_api/src/main/java/com/example/common_api/home_timeline/Tweet.kt
@@ -1,7 +1,18 @@
-package com.example.twittercloneappmvp.home.model
+package com.example.common_api.home_timeline
 
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+
+data class Tweet(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("created_at")
+    val createdAt: String,
+    @SerializedName("text")
+    val text: String,
+    @SerializedName("user")
+    val user: User
+) : Serializable
 
 data class User(
     @SerializedName("id")
