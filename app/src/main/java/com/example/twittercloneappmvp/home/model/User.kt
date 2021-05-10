541c626b6488435fbec5a56f90ef50da97ed90f0
shishamo <bnsgny@gmail.com>
2021-05-10 20:35:55 +0900

    add model (#3)

---
+++ app/src/main/java/com/example/twittercloneappmvp/home/model/User.kt
@@ -0,0 +1,17 @@
+package com.example.twittercloneappmvp.home.model
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class User(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("screen_name")
+    val screenName: String,
+    @SerializedName(value = "description", alternate = ["username"])
+    val description: String,
+    @SerializedName(value = "profile_image_url_https", alternate = ["profile_image_url"])
+    val profileImageUrlHttps: String,
+) : Serializable
