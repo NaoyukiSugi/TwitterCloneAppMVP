cd6ae38428c1cfb2c4def1082b2bfd83e952e482
Naoyuki Sugi <bnsgny@gmail.com>
2021-05-13 21:40:42 +0900

    add HomeViewHolderTest

---
+++ app/src/test/java/com/example/twittercloneappmvp/home/view/HomeViewHolderTest.kt
@@ -0,0 +1,17 @@
+package com.example.twittercloneappmvp.home.view
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class HomeViewHolderTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @Test
+    fun bind() {
+    }
+}
