397bc442002465c9cd1ef9dab59f0afd5c15139b
Naoyuki Sugi <bnsgny@gmail.com>
2021-05-19 19:57:06 +0900

    add api

---
+++ common_api/src/main/java/com/example/common_api/search_result/SearchResultResponse.kt
@@ -0,0 +1,47 @@
+package com.example.common_api.search_result
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class SearchResultResponse(
+    @SerializedName("data")
+    val tweets: List<SearchResultTweet>,
+    @SerializedName("includes")
+    val includes: UserList,
+    @SerializedName("meta")
+    val meta: Meta
+) : Serializable
+
+data class SearchResultTweet(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("created_at")
+    val createdAt: String,
+    @SerializedName("text")
+    val text: String,
+    @SerializedName("author_id")
+    val authorId: Long
+) : Serializable
+
+data class UserList(
+    @SerializedName("users")
+    val users: List<User>
+) : Serializable
+
+data class User(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("screen_name")
+    val screenName: String,
+    @SerializedName(value = "description", alternate = ["username"])
+    val description: String,
+    @SerializedName(value = "profile_image_url_https", alternate = ["profile_image_url"])
+    val profileImageUrlHttps: String,
+) : Serializable
+
+data class Meta(
+    @SerializedName("next_token")
+    val nextToken: String?
+) : Serializable
---
+++ common_api/src/main/java/com/example/common_api/search_result/SearchResultTimelineApi.kt
@@ -0,0 +1,17 @@
+package com.example.common_api.search_result
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+
+interface SearchResultTimelineApi {
+    @GET("2/tweets/search/recent")
+    suspend fun getSearchResultTimeline(
+        @Query("query") searchQuery: String,
+        @Query("expansions") expansions: String = "author_id",
+        @Query("tweet.fields") tweetField: String = "created_at",
+        @QueryMap(encoded = true) userFields: Map<String, String> = mapOf("user.fields" to "profile_image_url,description"),
+        @Query("next_token") nextToken: String? = null
+    ): Response<SearchResultResponse>
+}
