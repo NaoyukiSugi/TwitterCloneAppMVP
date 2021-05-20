a1bad7c3e633436503f7e1b81f2fb47ccae5c53d
shishamo <bnsgny@gmail.com>
2021-05-20 17:46:07 +0900

    add SearchResultViewProxy (#32)

--- app/src/main/java/com/example/twittercloneappmvp/feature/search_result/contract/SearchResultContract.kt
+++ app/src/main/java/com/example/twittercloneappmvp/feature/search_result/contract/SearchResultContract.kt
@@ -2,6 +2,8 @@ package com.example.twittercloneappmvp.feature.search_result.contract
 
 import com.example.common_api.search_result.SearchQuery
 import com.example.common_api.search_result.SearchResultTimelineResponse
+import com.example.twittercloneappmvp.feature.home.contract.HomeContract
+import com.example.twittercloneappmvp.model.Tweet
 import com.example.twittercloneappmvp.model.User
 import com.example.twittercloneappmvp.util.Result
 import kotlinx.coroutines.flow.Flow
@@ -25,4 +27,24 @@ interface SearchResultContract {
     interface IconClickListener {
         fun onIconClick(user: User)
     }
+
+    interface RefreshListener {
+        fun onRefresh()
+    }
+
+    interface ViewProxy {
+        fun initAdapter()
+        fun submitList(tweets: List<Tweet>)
+        fun showRecyclerView()
+        fun hideRecyclerView()
+        fun showErrorView()
+        fun hideErrorView()
+        fun showEmptyView()
+        fun hideEmptyView()
+        fun showLoadingView()
+        fun hideLoadingView()
+        fun setOnIconClickListener(listener: IconClickListener)
+        fun setOnRefreshListener(listener: RefreshListener)
+    }
+
 }
---
+++ app/src/main/java/com/example/twittercloneappmvp/feature/search_result/view/SearchResultViewProxy.kt
@@ -0,0 +1,87 @@
+package com.example.twittercloneappmvp.feature.search_result.view
+
+import android.view.View
+import android.widget.Button
+import androidx.annotation.VisibleForTesting
+import androidx.core.view.isVisible
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.twittercloneappmvp.R
+import com.example.twittercloneappmvp.feature.search_result.contract.SearchResultContract
+import com.example.twittercloneappmvp.model.Tweet
+
+class SearchResultViewProxy(private val fragment: Fragment) : SearchResultContract.ViewProxy {
+
+    @VisibleForTesting
+    internal lateinit var searchResultAdapter: SearchResultAdapter
+
+    private val recyclerView: RecyclerView?
+        get() = fragment.view?.findViewById(R.id.recycler_view)
+
+    private val errorView: View?
+        get() = fragment.view?.findViewById(R.id.error_view)
+
+    private val emptyView: View?
+        get() = fragment.view?.findViewById(R.id.empty_view)
+
+    private val refreshButton: Button?
+        get() = fragment.view?.findViewById(R.id.refresh_button)
+
+    private val progressBar: ContentLoadingProgressBar?
+        get() = fragment.view?.findViewById(R.id.progress_bar)
+
+    override fun initAdapter() {
+        createSearchResultAdapter().run {
+            searchResultAdapter = this
+            recyclerView?.adapter = this
+        }
+    }
+
+    override fun submitList(tweets: List<Tweet>) {
+        searchResultAdapter.submitList(tweets)
+    }
+
+    override fun showRecyclerView() {
+        recyclerView?.isVisible = true
+    }
+
+    override fun hideRecyclerView() {
+        recyclerView?.isVisible = false
+    }
+
+    override fun showErrorView() {
+        errorView?.isVisible = true
+    }
+
+    override fun hideErrorView() {
+        errorView?.isVisible = false
+    }
+
+    override fun showEmptyView() {
+        emptyView?.isVisible = true
+    }
+
+    override fun hideEmptyView() {
+        emptyView?.isVisible = false
+    }
+
+    override fun showLoadingView() {
+        progressBar?.isVisible = true
+    }
+
+    override fun hideLoadingView() {
+        progressBar?.isVisible = false
+    }
+
+    override fun setOnIconClickListener(listener: SearchResultContract.IconClickListener) {
+        searchResultAdapter.listener = listener
+    }
+
+    override fun setOnRefreshListener(listener: SearchResultContract.RefreshListener) {
+        refreshButton?.setOnClickListener { listener.onRefresh() }
+    }
+
+    @VisibleForTesting
+    internal fun createSearchResultAdapter() = SearchResultAdapter()
+}
