3da016cbf1b4d8ec7e4a87f4b4b2119d489ae367
Naoyuki Sugi <bnsgny@gmail.com>
2021-05-20 16:42:47 +0900

    add searchResultViewholder

--- app/src/main/java/com/example/twittercloneappmvp/search_result/contract/SearchResultContract.kt
+++ app/src/main/java/com/example/twittercloneappmvp/search_result/contract/SearchResultContract.kt
@@ -2,6 +2,7 @@ package com.example.twittercloneappmvp.search_result.contract
 
 import com.example.common_api.search_result.SearchQuery
 import com.example.common_api.search_result.SearchResultResponse
+import com.example.twittercloneappmvp.model.User
 import com.example.twittercloneappmvp.util.Result
 import kotlinx.coroutines.flow.Flow
 
@@ -11,5 +12,17 @@ interface SearchResultContract {
             searchQuery: SearchQuery,
             nextToken: String?
         ): Flow<Result<SearchResultResponse>>
+    }
+
+    interface ViewHolderViewProxy {
+        fun loadImageToView(imageUrl: String)
+        fun setName(name: String)
+        fun setScreenName(screenName: String)
+        fun setText(text: String)
+        fun setOnClickListener(user: User, listener: IconClickListener?)
+    }
+
+    interface IconClickListener {
+        fun onIconClick(user: User)
     }
 }
