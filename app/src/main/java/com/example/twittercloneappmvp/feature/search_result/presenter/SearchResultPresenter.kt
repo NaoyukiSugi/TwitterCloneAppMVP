f896f19aab37aeaf706d238021a4e07b68a45330
Naoyuki Sugi <bnsgny@gmail.com>
2021-05-20 18:16:40 +0900

    add presenter

---
+++ app/src/main/java/com/example/twittercloneappmvp/feature/search_result/presenter/SearchResultPresenter.kt
@@ -0,0 +1,47 @@
+package com.example.twittercloneappmvp.feature.search_result.presenter
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+import com.example.twittercloneappmvp.feature.search_result.contract.SearchResultContract
+import com.example.twittercloneappmvp.model.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+
+class SearchResultPresenter(
+    private val viewProxy: SearchResultContract.ViewProxy,
+    private val repository: SearchResultContract.Repository,
+    lifecycleOwner: LifecycleOwner
+) : SearchResultContract.Presenter, SearchResultContract.IconClickListener,
+    SearchResultContract.RefreshListener, LifecycleObserver, CoroutineScope by MainScope() {
+
+    init {
+        lifecycleOwner.lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onLifecycleEventOnStart() {
+        viewProxy.run {
+            initAdapter()
+            setOnIconClickListener(this@SearchResultPresenter)
+            setOnRefreshListener(this@SearchResultPresenter)
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onLifecycleEventOnDestroy() = cancel()
+
+    override fun getSearchResultTimeline() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onIconClick(user: User) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRefresh() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/test/java/com/example/twittercloneappmvp/feature/search_result/presenter/SearchResultPresenterTest.kt
@@ -0,0 +1,61 @@
+package com.example.twittercloneappmvp.feature.search_result.presenter
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import com.example.twittercloneappmvp.feature.search_result.contract.SearchResultContract
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.setMain
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.spy
+import org.mockito.kotlin.verify
+import kotlin.test.assertFalse
+
+internal class SearchResultPresenterTest {
+
+    private lateinit var presenter: SearchResultPresenter
+    private val viewProxy: SearchResultContract.ViewProxy = mock()
+    private val repository: SearchResultContract.Repository = mock()
+    private val lifecycle: Lifecycle = mock()
+    private val lifecycleOwner: LifecycleOwner = mock { on { lifecycle } doReturn lifecycle }
+
+    @BeforeEach
+    fun setUp() {
+        Dispatchers.setMain(TestCoroutineDispatcher())
+        presenter = spy(SearchResultPresenter(viewProxy, repository, lifecycleOwner))
+    }
+
+    // region onLifecycleEventOnStart
+    @Test
+    fun `onLifecycleEventOnStart should call initAdapter`() {
+        presenter.onLifecycleEventOnStart()
+
+        verify(viewProxy).initAdapter()
+    }
+
+    @Test
+    fun `onLifecycleEventOnStart should call setOnIconClickListener`() {
+        presenter.onLifecycleEventOnStart()
+
+        verify(viewProxy).setOnIconClickListener(presenter)
+    }
+
+    @Test
+    fun `onLifecycleEventOnStart should call setOnRefreshListener`() {
+        presenter.onLifecycleEventOnStart()
+
+        verify(viewProxy).setOnRefreshListener(presenter)
+    }
+    // endregion
+
+    @Test
+    fun `onLifecycleEventOnDestroy should call cancel`() {
+        presenter.onLifecycleEventOnDestroy()
+
+        assertFalse(presenter.isActive)
+    }
+}
