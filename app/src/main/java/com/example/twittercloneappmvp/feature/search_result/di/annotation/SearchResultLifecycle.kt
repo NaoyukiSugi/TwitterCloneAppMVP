e8ab42b68ab29d77e3774b1bb5aeb8e0fc7f3f55
Naoyuki Sugi <bnsgny@gmail.com>
2021-05-28 14:09:47 +0900

    fix

---
+++ app/src/main/java/com/example/twittercloneappmvp/feature/search_result/di/annotation/SearchResultLifecycle.kt
@@ -0,0 +1,7 @@
+package com.example.twittercloneappmvp.feature.search_result.di.annotation
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class SearchResultLifecycle
